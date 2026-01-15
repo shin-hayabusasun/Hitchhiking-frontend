c38c9920a6c9763beb589d7a99508fdde22986bc
shin-hayabusasun <156934097+shin-hayabusasun@users.noreply.github.com>
2026-01-15 10:19:13 +0900

    不要なマージコンフリクトのマーカーを削除

--- src/pages/hitch_hiker/MyRequest.tsx
+++ src/pages/hitch_hiker/MyRequest.tsx
@@ -97,7 +97,6 @@ const MyRequest = () => {
               />
             ))
           ) : (
-<<<<<<< HEAD
             <div className="flex flex-col items-center justify-center py-32 space-y-4">
               <div className="w-16 h-16 bg-gray-100 rounded-full flex items-center justify-center text-gray-300">
                 📁
